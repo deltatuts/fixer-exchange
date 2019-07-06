b63059366dc5fab34d07223062dbeea4a2e8620b
Victor Avelar <deltatuts@gmail.com>
2019-07-06 16:41:30 +0200

    Created API wrapper

---
+++ src/Exception/MissingAPIKeyException.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Deltatuts\Fixer\Exception;
+
+use Exception;
+
+/**
+ * Class MissingAPIKeyException
+ * @package Deltatuts\Fixer\Exception
+ */
+class MissingAPIKeyException extends Exception
+{
+    /**
+     * MissingAPIKeyException constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+            "You need to specify a non empty API key",
+            400,
+            null
+        );
+    }
+}
---
+++ src/FixerHttpClientTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Deltatuts\Fixer\Test;
+
+use Deltatuts\Fixer\Exception\MissingAPIKeyException;
+use Deltatuts\Fixer\FixerHttpClient;
+use GuzzleHttp\ClientInterface;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class FixerHttpClientTest
+ * @package Deltatuts\Fixer\Test
+ */
+class FixerHttpClientTest extends TestCase
+{
+    /**
+     * @throws MissingAPIKeyException
+     */
+    public function testFixerHttpClientIsAValidImplementationOfClientInterface()
+    {
+        $client = new FixerHttpClient('some-key');
+
+        $this->assertInstanceOf(ClientInterface::class, $client);
+    }
+
+    /**
+     * @throws MissingAPIKeyException
+     */
+    public function testMissingAPIKeyExceptionIsThrownWhenTheAPIKeyIsEmpty()
+    {
+        $this->expectException(MissingAPIKeyException::class);
+        new FixerHttpClient('');
+    }
+
+    /**
+     * @throws MissingAPIKeyException
+     */
+    public function testConfigIsOverwrittenWithCustomConfig()
+    {
+        $client = new FixerHttpClient('asdasdad', ['timeout' => 10, 'base_uri' => 'https://victoravelar.com']);
+        $this->assertArrayHasKey('timeout', $client->getConfig());
+        $this->assertArrayHasKey('base_uri', $client->getConfig());
+        $this->assertEquals(10, $client->getConfig('timeout'));
+        $this->assertEquals('https://victoravelar.com', $client->getConfig('base_uri'));
+    }
+}
